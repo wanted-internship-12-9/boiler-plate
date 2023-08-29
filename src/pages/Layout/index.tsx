5ca0fa73fd555e665c31536de14730c5a7af8056
JaydenLee1116 <vv55adss@gmail.com>
2023-08-30 00:05:50 +0900

    ✨ feat: Layout 컴포넌트 작성

---
+++ src/pages/Layout/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import * as S from './Layout.styled';
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+const Layout = ({ children }: LayoutProps) => {
+  return <S.Layout>{children}</S.Layout>;
+};
+
+export default Layout;
