77d1cc0fe4cc1e1692b6ff8d60d5b34f86e54e48
JaydenLee1116 <vv55adss@gmail.com>
2023-08-30 00:00:13 +0900

    💄 design: theme 적용

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 
 import { GlobalStyles } from './styles/GlobalStyles.styled';
 import Router from './routes/Router';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './styles/theme.styled';
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      <Router />
+      <ThemeProvider theme={theme}>
+        <Router />
+      </ThemeProvider>
     </>
   );
 }
---
+++ src/styles/theme.styled.ts
@@ -0,0 +1,10 @@
+import { color } from './color.styled';
+
+export const theme = {
+  colors: {
+    default: color.gray[400],
+    inverse: color.white,
+    primary: color.green[400],
+    secondary: color.red[400],
+  },
+};
