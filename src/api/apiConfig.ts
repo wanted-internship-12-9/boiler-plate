988fcf0544c48ffd27f788a986ea6aaa23aee3e4
JaydenLee1116 <vv55adss@gmail.com>
2023-08-29 22:26:11 +0900

    ✨ feat: github issues api 수정
    - facebook/react 뿐 아니라 다른 repo issues도 조회 가능하게 확장

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -1,10 +1,6 @@
 export const BASE_GITHUB_API_URL = 'https://api.github.com/repos';
 
 export const GITHUB_API_PATH = Object.freeze({
-  FACEBOOK: {
-    REACT: {
-      getIssues: (page: number) =>
-        `${BASE_GITHUB_API_URL}/facebook/react/issues?per_page=10&page=${page}`,
-    },
-  },
+  getIssues: (owner: string, repo: string, page: number) =>
+    `${BASE_GITHUB_API_URL}/${owner}/${repo}/issues?per_page=10&page=${page}`,
 });
