e682f17937dfcb1ddaaff2f92e803b3788425c3b
JaydenLee1116 <vv55adss@gmail.com>
2023-08-30 19:53:21 +0900

    ✨ feat: getKoreanDate 유틸함수 작성
    - 기존 Date 형식의 string에서 연, 월, 일 한국날짜 string 반환

---
+++ src/utils/getKoreanDate.ts
@@ -0,0 +1,9 @@
+// date: "2021-10-10T00:00:00.000Z"
+export const getKoreanDate = (date: string) => {
+  const dateObj = new Date(date);
+  const year = dateObj.getFullYear();
+  const month = dateObj.getMonth() + 1;
+  const day = dateObj.getDate();
+
+  return `${year}년 ${month}월 ${day}일`;
+};
