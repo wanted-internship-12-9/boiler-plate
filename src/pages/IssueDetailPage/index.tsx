66380abc9526a3619fd6be2e1953356b3d50038d
JaydenLee1116 <vv55adss@gmail.com>
2023-08-30 13:01:35 +0900

    🚚 rename: 기존 이슈관련 페이지 컴포넌트 이름 수정
    - 페이지 개념이 아닌 컴포넌트 개념으로 수정

--- src/pages/IssueDetailPage/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-// import * as S from './IssueDetailPage.style';
-
-const IssueDetailPage = () => {
-  return <div>이슈 디테일 페이지입니다.</div>;
-};
-
-export default IssueDetailPage;
--- src/pages/IssueListPage/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-// import * as S from './IssueListPage.style';
-
-const IssueListPage = () => {
-  return <div>이슈 리스트 페이지입니다.</div>;
-};
-
-export default IssueListPage;
